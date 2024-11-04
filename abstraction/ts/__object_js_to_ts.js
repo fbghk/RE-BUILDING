849e31c1adc8d7f58dc210631ce2327fe5bfe47a
fbghk <im706374@gmail.com>
2024-11-04 12:24:57 +0900

    Fix | __object_js_to_ts.js, ts
    class 형태로 변경

--- abstraction/ts/__object_js_to_ts.js
+++ abstraction/ts/__object_js_to_ts.js
@@ -1,14 +1,16 @@
-// Samhundred 생성자 함수
-function Samhundred(name, students) {
-    this.schoolName = name; // 각 객체마다 다른 학교 이름을 설정 가능
-    this.schoolStudent = students; // 각 객체마다 다른 학생 배열을 설정 가능
-    this.smile = function () {
+var Samhundred = /** @class */ (function () {
+    function Samhundred(name, students) {
+        this.schoolName = name;
+        this.schoolStudent = students;
+    }
+    Samhundred.prototype.smile = function () {
         console.log(this.schoolStudent[0]);
     };
-}
+    return Samhundred;
+}());
 // Samhundred 객체를 각각 다른 데이터로 생성
-var school3 = new Samhundred('High School A', ['Alice', 'Bob', 'Charlie']);
-var school4 = new Samhundred('High School B', ['David', 'Emma', 'Frank']);
+var school1 = new Samhundred('High School A', ['Alice', 'Bob', 'Charlie']);
+var school2 = new Samhundred('High School B', ['David', 'Emma', 'Frank']);
 // 각 객체의 메서드를 호출
-school3.smile(); // 출력: Alice
-school4.smile(); // 출력: David
+school1.smile(); // 출력: Alice
+school2.smile(); // 출력: David
